23451cfc24534f827929667dd745a235cca659c5
Gigi1111 <airolg1111@gmail.com>
2019-10-15 13:33:39 +0800

    Updates

---
+++ precache-manifest.19072ab85ade1d0fbce24aa86b3f93ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4bfd46a45297d2f0a3d3eccd7852445",
+    "url": "/bitcoin/index.html"
+  },
+  {
+    "revision": "fbda993a2cdc3c0365f4",
+    "url": "/bitcoin/static/css/main.f80b2536.chunk.css"
+  },
+  {
+    "revision": "a2d30b2d96b51b694869",
+    "url": "/bitcoin/static/js/2.73e83acb.chunk.js"
+  },
+  {
+    "revision": "fbda993a2cdc3c0365f4",
+    "url": "/bitcoin/static/js/main.0e0fa8ba.chunk.js"
+  },
+  {
+    "revision": "626c86ab5fdc5f244925",
+    "url": "/bitcoin/static/js/runtime-main.ffab52a1.js"
+  }
+]);
