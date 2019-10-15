fda1fd5cdd382b883fa71c85aa7d3182b6ba645d
Gigi1111 <airolg1111@gmail.com>
2019-10-15 13:42:40 +0800

    update

--- src/data.js
+++ src/data.js
@@ -17,11 +17,11 @@ class Data extends React.Component {
 	}
 	formatChartDate(s) {
 		let months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
-		//2019-10-13
+
 		let str = s.split('');
 		let m = str[5] + str[6];
 		let d = str[8] + str[9];
-		//	console.log(months[Number(m) - 1] + ' ' + Number(d));
+
 		return months[Number(m) - 1] + ' ' + Number(d);
 	}
 	async getChartData() {
@@ -35,7 +35,6 @@ class Data extends React.Component {
 			.then((response) => response.json())
 			.then((obj) => {
 				for (let i = 0; i < this.state.datesFormatted.length; i++) {
-					//	console.log('inChartData:' + this.state.datesFormatted[i] + ':' + obj.bpi[this.state.datesFormatted[i]]);
 					dateArr.push(this.formatChartDate(this.state.datesFormatted[i]));
 					rateArr.push(obj.bpi[this.state.datesFormatted[i]]);
 				}
@@ -51,14 +50,11 @@ class Data extends React.Component {
 				});
 				let t = this.formatChartDate(this.getDateFormatted(new Date(), 0));
 				let r = obj.bpi.EUR.rate || this.state.currentRate;
-				console.log('todayData' + t + ':' + r + '(type)' + typeof r);
 				if (r.indexOf(',')) {
 					r = r.split('').filter((a) => a !== ',').join('');
 				}
-				console.log('num:' + r);
 				dateArr.push(t);
 				rateArr.push(parseFloat(r));
-				console.log(dateArr + rateArr);
 			});
 
 		this.setState({
@@ -72,7 +68,6 @@ class Data extends React.Component {
 				]
 			}
 		});
-		console.log(this.state);
 	}
 	getPass14DatesFormatted(today) {
 		let array = [];
@@ -85,10 +80,9 @@ class Data extends React.Component {
 			let dFormatted =
 				dYear + '-' + (dMonth >= 10 ? dMonth : '0' + dMonth) + '-' + (dDate >= 10 ? dDate : '0' + dDate);
 
-			//console.log(dFormatted);
 			array.push(dFormatted);
 		}
-		//console.log(array);
+
 		return array;
 	}
 	getDateFormatted(today, n) {
@@ -104,7 +98,6 @@ class Data extends React.Component {
 			'-' +
 			(startDate >= 10 ? startDate : '0' + startDate);
 
-		//	console.log(startFormatted);
 		return startFormatted;
 	}
 
@@ -122,7 +115,6 @@ class Data extends React.Component {
 				});
 				t = this.formatChartDate(this.getDateFormatted(new Date(), 0));
 				r = obj.bpi.EUR.rate;
-				console.log('~~' + t + ':' + r);
 			});
 	}
 
